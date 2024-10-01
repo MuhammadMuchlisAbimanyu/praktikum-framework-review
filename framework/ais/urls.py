4c2fbedb5bdda5b05ca4c5a076429419950d9b30
Muchlis Abi <amanyu851@gmail.com>
2024-10-01 12:22:51 +0800

    review 4

--- framework/ais/urls.py
+++ framework/ais/urls.py
@@ -4,4 +4,7 @@ from . import views  # Impor views untuk digunakan dalam routing
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('about', views.about, name='about'),  # Sesuaikan dengan string URL
+    path('student/', views.student_index, name='student_index'), #Read
+    path('student/create/', views.student_create, name='student_create'),# Create
 ]
+
--- framework/ais/views.py
+++ framework/ais/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import StudentsForm
+from .models import Students
 
 # Create your views here.
 def homepage(request):
@@ -12,3 +15,19 @@ def about(request):
         'about' : True
     }
     return render(request, 'homepage/about.html', context)
+
+# READ Mahasiswa
+def student_index(request):
+    students = Students.objects.all()
+    return render(request, 'student/index.html', {'students': students})
+
+def student_create(request):
+    if request.method == 'POST':
+        form = StudentsForm(request.POST)
+        if form.is_valid():
+            form.save()  # Simpan data mahasiswa ke database
+            messages.success(request, 'Mahasiswa berhasil dibuat!')  # Pesan sukses
+            return redirect('student_index')  # Redirect ke halaman index mahasiswa
+    else:
+        form = StudentsForm()  # Definisikan form jika metode bukan POST
+    return render(request, 'student/create.html', {'form': form})
