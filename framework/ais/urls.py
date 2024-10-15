1001b0843b8f3bf02565dad1ef22da3895f86164
Muchlis Abi <amanyu851@gmail.com>
2024-10-15 12:40:22 +0800

    Review 6

--- framework/ais/urls.py
+++ framework/ais/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views  # Impor views untuk digunakan dalam routing
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
@@ -8,5 +9,11 @@ urlpatterns = [
     path('student/create/', views.student_create, name='student_create'),# Create
     path('student/update/<int:student_id>/', views.student_update, name='student_update'),
     path('student/delete/<int:student_id>', views.student_delete, name='student_delete'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/admin', views.dashboard_admin, name='dashboard_admin'),
+    path('dashboard/student', views.dashboard_student,name='dashboard_student'),
+    path('dashboard/teacher', views.dashboard_teacher,name='dashboard_teacher'),
 ]
 
--- framework/ais/views.py
+++ framework/ais/views.py
@@ -3,6 +3,9 @@ from django.contrib import messages
 from .forms import StudentsForm
 from .models import Students
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse, HttpResponseForbidden
+from .decorators import group_required
 
 # Create your views here.
 def homepage(request):
@@ -53,3 +56,37 @@ def student_delete(request, student_id):
     messages.success(request, 'Data mahasiswa berhasil dihapus')
     return JsonResponse({'success': True})
 
+@login_required
+def dashboard(request):
+    user = request.user
+    if user.groups.filter(name='Admin').exists():
+        return redirect('dashboard_admin')
+    elif user.groups.filter(name='Student').exists():
+        return redirect('dashboard_student')
+    elif user.groups.filter(name='Teacher').exists():
+        return redirect('dashboard_teacher')
+    return HttpResponseForbidden("You do not have permission to access this page.")
+
+@login_required
+def dashboard_admin(request):
+    return render(request, 'dashboard/admin.html')
+
+@login_required
+def dashboard_student(request):
+    return render(request, 'dashboard/student.html')
+
+@login_required
+def dashboard_teacher(request):
+    return render(request, 'dashboard/teacher.html')
+
+@group_required('Admin')
+def dashboard_admin(request):
+    return render(request, 'dashboard/admin.html')
+
+@group_required('Student')
+def dashboard_student(request):
+    return render(request, 'dashboard/student.html')
+
+@group_required('Teacher')
+def dashboard_teacher(request):
+    return render(request, 'dashboard/teacher.html')
